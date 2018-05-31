b51e3f1d3393ca5bd6d473ddadd330c8e5d44392
York Wu <dmpig0318@gmail.com>
2018-06-01 02:35:17 +0800

    init widget

---
+++ app/src/main/java/yorkdev/sample/stust/day6/SmallWidgetProvider.java
@@ -0,0 +1,43 @@
+package yorkdev.sample.stust.day6;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class SmallWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        Log.i("111", "onReceive");
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Log.i("111", "onUpdate");
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        Log.i("111", "onAppWidgetOptionsChanged");
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        Log.i("111", "onDeleted");
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        Log.i("111", "onEnabled");
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        Log.i("111", "onDisabled");
+    }
+}
