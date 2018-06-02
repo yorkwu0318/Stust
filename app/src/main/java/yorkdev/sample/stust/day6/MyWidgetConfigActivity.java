78191f9a0fecdcb38fed5836d4f393b14c87da3a
York Wu <dmpig0318@gmail.com>
2018-06-02 09:08:37 +0800

    add configWidgetActivity

---
+++ app/src/main/java/yorkdev/sample/stust/day6/MyWidgetConfigActivity.java
@@ -0,0 +1,13 @@
+package yorkdev.sample.stust.day6;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MyWidgetConfigActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
