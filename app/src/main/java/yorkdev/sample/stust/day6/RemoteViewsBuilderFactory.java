6746ba94c1fb8cc556f52c9305ae8712c93c8ab2
York Wu <dmpig0318@gmail.com>
2018-06-02 09:03:57 +0800

    add RemoteViewsFactory for widget

---
+++ app/src/main/java/yorkdev/sample/stust/day6/RemoteViewsBuilderFactory.java
@@ -0,0 +1,40 @@
+package yorkdev.sample.stust.day6;
+
+
+import android.appwidget.AppWidgetManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import yorkdev.sample.stust.AirQuality;
+
+public class RemoteViewsBuilderFactory {
+
+    public interface RemoteViewsBuilder {
+        RemoteViewsBuilder setAirQuality(AirQuality airQuality);
+        RemoteViews create();
+    }
+
+    public static RemoteViewsBuilder create(Bundle widgetOption) {
+
+        int minWidth = widgetOption.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+        int minHeight = widgetOption.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+
+        int rows = getCellsForSize(minHeight);
+        int columns = getCellsForSize(minWidth);
+
+        Log.i("111", "rows="+rows+" columns="+columns);
+
+        if (columns > 2) {
+            return new RichRemoteViewsBuilder();
+        } else {
+            return new SimpleRemoteViewsBuilder();
+        }
+    }
+
+    private static int getCellsForSize(int size) {
+        return (int)(Math.ceil(size + 30d)/70d);
+    }
+}
+
+
